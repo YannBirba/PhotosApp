a17d5d461f57e4a095731e39cae5a2e2931e6c0d
YannBirba <birbayann@gmail.com>
2021-11-09 11:52:42 +0100

    Starting guard is loggin

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/shared/guards/auth-guard.guard';
 import { LoginViewComponent } from './components/login-view/login-view.component';
 import { RegisterViewComponent } from './components/register-view/register-view.component';
 import { UserViewComponent } from './components/user-view/user-view.component';
@@ -7,11 +8,11 @@ import { UserViewComponent } from './components/user-view/user-view.component';
 const routes: Routes = [
   { path: 'login', component: LoginViewComponent },
   { path: 'register', component: RegisterViewComponent },
-  { path: '', component: UserViewComponent },
+  { path: '', component: UserViewComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class UserRoutingModule { }
+export class UserRoutingModule {}
--- src/shared/guards/auth-guard.guard.ts
+++ src/shared/guards/auth-guard.guard.ts
@@ -2,42 +2,48 @@ import { Injectable, OnDestroy, OnInit } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanDeactivate,
+  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { AuthState } from 'src/models/auth-state/auth-state.model';
 import { AuthService } from '../services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate, OnDestroy, OnInit {
-  private readonly onDestroy = new Subject<void>();
-  // private authState: AuthState;
-  constructor(private authService: AuthService) {}
+export class AuthGuard implements CanActivate {
+  private unsubscribe$ = new Subject<void>();
+  private authState?: AuthState;
+  constructor(private authService: AuthService, private router: Router) { }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): boolean {
-    if (1 == 1) {//this.authService.isLogIn = true
-      if (route.url[0].path === 'admin') {
+    this.authService.isLoggedIn()
+    .pipe(takeUntil(this.unsubscribe$))
+    .subscribe(response => {
+      this.authState = response;
+    });
+    if (this.authState) {
+      if (this.authState.is_logged_in) {
+        console.log('AuthGuard: canActivate: true');
+        return true;
+      }
+      else {
+        this.router.navigate(['/user/login']);
+        return false;
+      }
+    }
+    
+      // if (route.url[0].path === 'admin') {
         // if (this.authService.isLoggedIn) {
         //   return true;
         // }
-      }
-      return true;
-    }
+      // }
     return false;
-  }
-  ngOnInit() {
-    // this.authService.isLoggedIn() 
-    //   .pipe(takeUntil(this.onDestroy))
-    //   .subscribe(response => {
-    //   this.authState = response;
-    // });
-  }
-  ngOnDestroy() {
-    this.onDestroy.next();
-  }
+    }
 }
